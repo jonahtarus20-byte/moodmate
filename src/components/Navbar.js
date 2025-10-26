c6aa26eb148cb24484036e97f9aaa71227918a16
Your Name <youremail@example.com>
2025-10-26 16:19:18 +0300

    finished project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("user"));
 
   const handleSignOut = () => {
     localStorage.removeItem("user");
-    window.location.href = "/login";
+    navigate("/login");
   };
 
   return (
