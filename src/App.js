5ba9808aa557c49cacff16e5eb9d0b6162c29396
Your Name <youremail@example.com>
2025-10-28 21:49:15 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Journal from "./pages/Journal";
 import Profile from "./pages/Profile";
@@ -6,8 +11,11 @@ import Login from "./pages/Login";
 import EditProfile from "./pages/EditProfile";
 
 function App() {
+  const isGitHub = window.location.hostname.includes("github.io");
+  const Router = isGitHub ? HashRouter : BrowserRouter;
+
   return (
-    <Router>
+    <Router basename={isGitHub ? "/moodmate" : "/"}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/journal" element={<Journal />} />
